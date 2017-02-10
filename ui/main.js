f5fb294e4cd460bd6e749a8beabfdeede98d2ca8
JacobRoy <jacobroy8597@gmail.com>
2017-02-10 22:19:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,11 +18,23 @@ var nameInput=document.getElementById('name');
 var name= nameInput.value;
 var submit=document.getElementById('submit-btn');
 submit.onclick=function() {
-    var names=['name1','name2','name3','name4'];
+     var request= new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names=request.responsetext;
+                names=JSON.parse(names);
     var list='';
     for(var i=o;i<names.length;i++){
         list+= '<li>' + names[i] + '</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+               
+            }
+        }
+        };
+        request.span('GET','http://jacobroy.imad.hasura-app.io/submit-name?name='+name,true);
+        request.send(null);
+    
 };
